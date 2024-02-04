e6aaf6a34e5c707716132e1d69d8e8dd1d5369e6
MTLKS <matthew.liew99@gmail.com>
2024-02-04 14:44:25 +0000

    fix redis

--- srcs/requirements/bonus/redis/tools/entrypoint.sh
+++ srcs/requirements/bonus/redis/tools/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [ ! -f "/etc/redis.conf.bak" ]; then
-    cp /etc/redis.conf /etc/redis.conf.bak
-    sed -i "s|# maxmemory <bytes>|maxmemory 2mb|g" /etc/redis.conf
-    sed -i "s|# maxmemory-policy noeviction|maxmemory-policy allkeys-lru|g" /etc/redis.conf
+if [ ! -f "/etc/redis/redis.conf.bak" ]; then
+    cp /etc/redis/redis.conf /etc/redis/redis.conf.bak
+    sed -i "s|# maxmemory <bytes>|maxmemory 2mb|g" /etc/redis/redis.conf
+    sed -i "s|# maxmemory-policy noeviction|maxmemory-policy allkeys-lru|g" /etc/redis/redis.conf
 fi
 
 exec "$@"
