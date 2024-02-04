be6e5a175b7efbeedddec4d9f2dfd21a2be5f98e
MTLKS <matthew.liew99@gmail.com>
2024-02-04 15:07:47 +0000

    add persistence in prometheus

--- srcs/requirements/tools/reset.sh
+++ srcs/requirements/tools/reset.sh
@@ -3,5 +3,5 @@ docker rm $(docker ps -qa)
 docker rmi -f $(docker images -qa)
 docker volume rm $(docker volume ls -q)
 docker network rm $(docker network ls -q) 2>/dev/null
-sudo rm -rvf /home/maliew/data/db/* /home/maliew/data/files/*
+sudo rm -rvf /home/maliew/data/db/* /home/maliew/data/files/* /home/maliew/data/prometheus/*
 exit 0
