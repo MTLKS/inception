187e4152d6aa93fd00c918c3deb764c747ec22c5
MTLKS <matthew.liew99@gmail.com>
2024-02-04 14:45:09 +0000

    fix ssl generate

--- srcs/requirements/tools/ssl.sh
+++ srcs/requirements/tools/ssl.sh
@@ -1,5 +1,9 @@
 # get env variables from env file
 export $(grep -v '^#' srcs/.env | xargs)
 
+if [ ! -d "srcs/requirements/nginx/ssl" ]; then
+	mkdir -p srcs/requirements/nginx/ssl
+fi
+
 # generate ssl certificate
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout srcs/requirements/nginx/ssl/$DOMAIN_NAME.key -out srcs/requirements/nginx/ssl/$DOMAIN_NAME.crt -subj "/C=MY/ST=Kuala Lumpur/L=Kuala Lumpur/O=42KL/OU=42KL/CN=$DOMAIN_NAME"
